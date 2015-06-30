c7e5f72f22a661b80e339f4db905dec98924a47c
Nico Prananta <nicnocquee@yahoo.com>
2015-06-30 10:17:19 +0900

    Initial Commit

---
+++ ComposerSheet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComposerSheet
+//
+//  Created by Nico Prananta on 6/30/15.
+//  Copyright (c) 2015 DelightfulDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
